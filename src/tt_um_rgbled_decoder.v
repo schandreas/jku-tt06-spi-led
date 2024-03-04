707410444154364aec4e562aca456f8e14663e30
schandreas <andreas@restental.at>
2024-03-04 21:03:36 +0100

    Revert to 8 LEDs due to GDS

--- src/tt_um_rgbled_decoder.v
+++ src/tt_um_rgbled_decoder.v
@@ -14,7 +14,7 @@
 
 `default_nettype none
 
-module tt_um_rgbled_decoder #(parameter LEDS = 10, parameter BITS_PER_LED = 24) (
+module tt_um_rgbled_decoder #(parameter LEDS = 8, parameter BITS_PER_LED = 24) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
